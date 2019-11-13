410051b6044b118a454ffd4a7c6c615b0f563f8a
Diana-Teodora Vrabie <dianateodorav@gmail.com>
2019-11-13 11:04:59 +0200

    read food types

--- main.c
+++ main.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_FOOD_NAME 20
 
 int main() {
-    printf("Hello, World!\n");
+    int noFoodTypes;
+    printf("Please input the number of food types\n");
+    scanf("%d", &noFoodTypes);
+    getchar();
+    char **foodTypes;
+    foodTypes = (char**)malloc(noFoodTypes * sizeof(char*));
+    printf("Please food types (each on a new line, may contain spaces)\n");
+    for (int i = 0; i < noFoodTypes; i++) {
+        foodTypes[i] = (char*)malloc(MAX_FOOD_NAME* sizeof(char));
+        gets(foodTypes[i]);
+    }
+    for (int i = 0; i < noFoodTypes; i++) {
+        printf("%s\n", foodTypes[i]);
+    }
+
+
+    //memory deallocation
+
+    free(foodTypes);
+    
     return 0;
 }
